9bfaaaec079a8284eb39eeb047221b9b718c2738
Nicolas Bossard <nicolas.bossard@orange.com>
2023-11-27 10:19:22 +0100

    Adding plugin gx to open links

--- init.lua
+++ init.lua
@@ -171,6 +171,35 @@ function plug_tcomment()
 end
  -- }}}
 
+-- {{{ gx : open links under cursor
+-- see : https://github.com/chrishrb/gx.nvim
+function plug_gx
+  return {
+    "chrishrb/gx.nvim",
+    event = { "BufEnter" },
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true, -- default settings
+
+    -- you can specify also another config if you want
+    config = function() require("gx").setup {
+      open_browser_app = "open", -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
+      open_browser_args = { "--background" }, -- specify any arguments, such as --background for macOS' "open".
+      handlers = {
+        plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
+        github = true, -- open github issues
+        brewfile = true, -- open Homebrew formulaes and casks
+        package_json = true, -- open dependencies from package.json
+        search = true, -- search the web/selection on the web if nothing else is found
+      },
+      handler_options = {
+        search_engine = "google", -- you can select between google, bing, duckduckgo, and ecosia
+        search_engine = "https://search.brave.com/search?q=", -- or you can pass in a custom search engine
+      },
+    } end,
+  }
+end
+ -- }}}
+
 -- {{{ chatGPT : plugin to use OpenAI GPT-3 to generate text
 -- see https://github.com/jackMort/ChatGPT.nvim
 function plug_chatGPT()
@@ -263,6 +292,7 @@ require("lazy").setup({
   plug_tcomment(),
   plug_x_go(),
   plug_telescope(),
+  plug_gx(),
   plug_chatGPT(),
   plug_color_scheme_gruvbox(),
   plug_color_scheme_dracula(),
