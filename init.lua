c349e52fe495724fe2d0005de4d4ffe0b13a20a7
Nicolas Bossard <nicolas.bossard@orange.com>
2024-06-12 23:16:55 +0200

    improve treesiter config

--- init.lua
+++ init.lua
@@ -160,11 +160,46 @@ end
 -- see : https://github.com/nvim-treesitter/nvim-treesitter
 -- Usage : TSInstallInfo
 -- Usage : TSInstall <language>
+-- Puis : TSEnable
 -- :TSUpdate all
 local function plug_treesitter()
   return {
     "nvim-treesitter/nvim-treesitter",
     event="BufRead",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = { "lua", "go" },
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = false,
+        -- List of parsers to ignore installing (or "all")
+        ignore_install = {},
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = true,
+        highlight = {
+          enable = true,
+        },
+        indent = {
+          enable = false
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<CR>", -- set to `false` to disable one of the mappings
+            scope_incremental = "<CR>",
+            node_incremental = "<TAB>",
+            node_decremental = "<S-TAB>",
+          },
+        },
+        modules ={}
+      }
+
+      -- treesitter breaks foldmethod=syntax (sic)
+      -- but works like this
+      vim.cmd("autocmd Filetype go set foldmethod=expr")
+      vim.cmd("autocmd Filetype go set foldexpr=nvim_treesitter#foldexpr()")
+    end
   }
 end
 --}}}
