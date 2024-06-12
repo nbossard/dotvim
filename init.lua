2269f70edee71e26f6b1a90dac2f8ab0da91b145
Nicolas Bossard <nicolas.bossard@orange.com>
2024-06-12 23:22:00 +0200

    changing register behaviour

--- init.lua
+++ init.lua
@@ -983,3 +983,23 @@ function DevMode()
   vim.cmd('COQnow')
 end
 vim.cmd('command! -nargs=0 Dev lua DevMode()')
+
+-- changing vim default behaviour on registers
+-- make register 1-9 contain also yank history
+-- not only deleted history
+vim.cmd([[
+function! SaveLastReg()
+    if v:event['regname']==""
+        if v:event['operator']=='y'
+            for i in range(8,1,-1)
+                exe "let @".string(i+1)." = @". string(i) 
+            endfor
+            if exists("g:last_yank")
+                let @1=g:last_yank
+            endif
+            let g:last_yank=@"
+        endif 
+    endif
+endfunction 
+:autocmd TextYankPost * call SaveLastReg()
+]])
