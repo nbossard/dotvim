0383eb6a5b4e0ee4578be8f9ed0d009dec975ef2
Nicolas Bossard <nicolas.bossard@orange.com>
2024-06-12 23:22:50 +0200

    adding gofmt on save

--- init.lua
+++ init.lua
@@ -731,6 +731,18 @@ local function plug_x_go()
       },
       config = function()
         require("go").setup()
+        -- foldmethod is set by Treesitter 
+
+        -- Run gofmt on save
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          pattern = "*.go",
+          callback = function()
+            require('go.format').gofmt()
+          end,
+          group = format_sync_grp,
+        })
+
       end,
       ft = {"go", 'gomod'}, -- Lazy load on filetype go
       build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
