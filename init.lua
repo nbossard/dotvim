e39646eb118c349055fd08c96e3ba0dbd7732617
Nicolas Bossard <nicolas.bossard@orange.com>
2024-09-05 10:24:29 +0200

    adding auto install of treesitter for css

--- init.lua
+++ init.lua
@@ -330,7 +330,7 @@ local function plug_treesitter()
     config = function()
       require'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "lua", "go" , "java", "bash", "html", "javascript"},
+        ensure_installed = { "lua", "go" , "java", "bash", "html", "css", "javascript"},
         -- Automatically install missing parsers when entering buffer
         -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
         auto_install = false,
