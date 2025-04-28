ebbbe1bef672905d2f5488b23a7697eb870c2359
Nicolas Bossard <nicolas.bossard@orange.com>
2025-04-28 17:39:38 +0200

    adding llm codestral

--- init.lua
+++ init.lua
@@ -932,7 +932,17 @@ local function plug_avante()
     version = false, -- set this if you want to always pull the latest change
     opts = {
       behaviour = {
-          auto_suggestions = true, -- Experimental stage
+          auto_suggestions = false, -- Experimental stage
+      },
+      auto_suggestions_provider = "codestral-llmproxy",
+      provider = "claude-llmproxy", -- Recommend using Claude
+      openai = {
+        endpoint = "https://api.openai.com/v1",
+        model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+        timeout = 30000, -- timeout in milliseconds
+        temperature = 0, -- adjust if needed
+        max_tokens = 4096,
+        -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
       },
       auto_suggestions_provider = "openai-llmproxy", 
       provider = "openai", -- Recommend using Claude
@@ -959,6 +969,14 @@ local function plug_avante()
           timeout = 30000, -- Timeout in milliseconds
           temperature = 0,
           max_tokens = 8000,
+        },
+        ["codestral-llmproxy"] = {
+          endpoint = "https://llmproxy.ai.orange",
+          __inherited_from = "openai",
+          model = "vertex_ai/codestral",
+          timeout = 2000, -- Timeout in milliseconds
+          temperature = 0,
+          max_tokens = 128000,
         },
       },
     },
