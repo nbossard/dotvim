63d6148a24c49af55dfeb449a4737de3a7d8bc2b
Nicolas Bossard <nicolas.bossard@orange.com>
2025-07-31 14:24:35 +0200

    adding plugin treesitter-context

--- init.lua
+++ init.lua
@@ -459,6 +459,32 @@ local function plug_treesitter()
 end
 --}}}
 
+-- {{{ treesj: plugin to keep function name line on top
+-- see https://github.com/nvim-treesitter/nvim-treesitter-context
+local function plug_treesitter_context()
+  return {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require'treesitter-context'.setup{
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = false, -- Enable multiwindow support.
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = 'cursor',  -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = '―',
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      }
+    end
+  }
+end
+-- }}}
+
 -- {{{ treesj: plugin to split or join lines of code with intelligence
 -- see : https://github.com/Wansmer/treesj
 local function plug_treesj()
@@ -1615,6 +1641,7 @@ require("lazy").setup({
   plug_ale(),
   plug_mark(),
   plug_treesitter(),
+  plug_treesitter_context(),
   plug_treesj(),
   plug_trouble(),
   plug_nvim_tree(),
