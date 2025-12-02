61ba8bfe45266ef8797ae08381a2a9feda66f9c0
Nicolas Bossard <nicolas.bossard@orange.com>
2025-12-02 11:47:26 +0100

    adding plugin nvim-dap

--- init.lua
+++ init.lua
@@ -924,6 +924,66 @@ local function plug_mason()
 end
 -- }}}
 
+-- {{{ nvim-dap : Debug Adapter Protocol client implementation
+-- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#javascript
+-- note: add debug adapter for nodejs using mason
+local function plug_nvim_dap()
+  return {
+    "mfussenegger/nvim-dap",
+    config = function ()
+      local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          -- 💀 Make sure to update this path to point to your installation
+          args = {vim.fn.stdpath('data') .. '/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js', "${port}"},
+        }
+      }
+
+      for _, language in ipairs(js_based_languages) do
+        require("dap").configurations[language] = {
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Launch file",
+            program = "${file}",
+            cwd = "${workspaceFolder}",
+          },
+          -- mocha config for node.js testing
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Mocha Tests",
+            program = "${workspaceFolder}/node_modules/mocha/bin/_mocha",
+            args = { "--timeout", "999999", "--colors", "${file}" },
+            cwd = "${workspaceFolder}"
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach",
+            processId = require 'dap.utils'.pick_process,
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-chrome",
+            request = "launch",
+            name = "Start Chrome with \"localhost\"",
+            url = "http://localhost:3000",
+            webRoot = "${workspaceFolder}",
+            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
+          }
+        }
+      end
+    end
+  }
+end
+-- }}}
+
 -- {{{ nvim-dap-ui : Debug Adapter Protocol UI
 -- see : https://github.com/rcarriga/nvim-dap-ui
 -- A richer frienly UI for nvim-dap
@@ -2046,60 +2106,7 @@ require("lazy").setup({
   plug_rgflow(),
   plug_jsonpath(),
   plug_mason(),
-  -- https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#javascript
-  -- note: add debug adapter for nodejs using mason
-  {
-    "mfussenegger/nvim-dap",
-    config = function ()
-      local js_based_languages = { "typescript", "javascript", "typescriptreact" }
-
-      require("dap-vscode-js").setup({
-        -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-        debugger_path = "/Users/nbossard/my-security-partner/vscode-js-debug", -- Path to vscode-js-debug installation.
-        -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-        adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost' }, -- which adapters to register in nvim-dap
-        -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-        -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-        -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-      })
-
-      for _, language in ipairs(js_based_languages) do
-        require("dap").configurations[language] = {
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Launch file",
-            program = "${file}",
-            cwd = "${workspaceFolder}",
-          },
-          -- mocha config for node.js testing
-          {
-            type = "pwa-node",
-            request = "launch",
-            name = "Mocha Tests",
-            program = "${workspaceFolder}/node_modules/mocha/bin/_mocha",
-            args = { "--timeout", "999999", "--colors", "${file}" },
-            cwd = "${workspaceFolder}"
-          },
-          {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require 'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}",
-          },
-          {
-            type = "pwa-chrome",
-            request = "launch",
-            name = "Start Chrome with \"localhost\"",
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir"
-          }
-        }
-      end
-    end
-  },
+  plug_nvim_dap(),
   plug_nvim_dap_ui(),
   plug_minimap(),
   plug_scrollbar(),
