963526226533f9a3bffbdb294d79dd1a4413f1dc
Nicolas Bossard <nicolas.bossard@orange.com>
2025-12-31 14:37:33 +0100

    configure mcp hub for avante

--- init.lua
+++ init.lua
@@ -1454,7 +1454,13 @@ local function plug_mcphub()
     -- uncomment this if you don't want mcp-hub to be available globally or can't use -g
     -- build = "bundled_build.lua",  -- Use this and set use_bundled_binary = true in opts  (see Advanced configuration)
     config = function()
-      require("mcphub").setup()
+      require("mcphub").setup({
+        extensions = {
+          avante = {
+            make_slash_commands = true, -- make /slash commands from MCP server prompts
+          }
+        }
+      })
     end,
 }
 end
